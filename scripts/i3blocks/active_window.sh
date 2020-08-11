01d0e1463db999608345cdbbbf79c7139f315f1b
Bodo <bodo.akdeniz@gmail.com>
2020-08-11 17:56:25 +0200

    Anzeigefehler behoben

--- scripts/i3blocks/active_window.sh
+++ scripts/i3blocks/active_window.sh
@@ -10,12 +10,14 @@ declare -A FIELDS
 xtitle -s -f '%u\n' | while read ID; do
 	XPROP=$(xprop -id $ID)
 	CLASS=$(echo "$XPROP" | grep 'WM_CLASS(STRING)' | sed -r 's/.*"(.*?)".*?/\1/')
-	NAME=$(echo "$XPROP" | grep '_NET_WM_ICON_NAME(UTF8_STRING)' | egrep -e '.*"(.*?)".*?'| sed -r 's/.*"(.*?)".*?/\1/')
-	if [ -z "$NAME" ]; then
+    NAME=$(echo "$XPROP" | grep '_NET_WM_ICON_NAME(UTF8_STRING)')
+    NAME=${NAME#*=\ \"}
+    NAME=${NAME%\"}
+	if [[ "$NAME" =~ ^_NET_WM_ICON_NAME\(UTF8_STRING\) ]]; then
 		FIELDS["title"]="$CLASS"
 	else
 		FIELDS["title"]="$NAME"
 	fi
-	OUTPUT=$(format_output "$FORMAT")
-	pango_markup "$OUTPUT" "$(get_color_by_perc 100)"
+    FIELDS["color"]="#999999"
+	style_output "$(format_output "$FORMAT")"
 done 
